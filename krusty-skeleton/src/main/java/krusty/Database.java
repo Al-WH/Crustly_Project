fbdd7c677ff41d3546ee06b93cf12af563ac3cda
Alaa Wahbah <76135619+Al-WH@users.noreply.github.com>
2021-04-15 13:01:05 +0200

    conn

--- krusty-skeleton/src/main/java/krusty/Database.java
+++ krusty-skeleton/src/main/java/krusty/Database.java
@@ -3,6 +3,9 @@ package krusty;
 import spark.Request;
 import spark.Response;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
@@ -16,17 +19,27 @@ public class Database {
 	private static final String jdbcString = "jdbc:mysql://localhost/krusty";
 
 	// For use with MySQL or PostgreSQL
-	private static final String jdbcUsername = "<CHANGE ME>";
-	private static final String jdbcPassword = "<CHANGE ME>";
+	private static final String jdbcUsername = "db04";
+	private static final String jdbcPassword = "dif639he";
+	private Connection connection;
 
 	public void connect() {
 		// Connect to database here
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://" + databaseServerAddress + "/" + database,
+					databaseUser, databasePassword);
+		} catch (SQLException e) {
+			handleSQLException(e);
+			return false;
+		}
 	}
 
 	// TODO: Implement and change output in all methods below!
 
 	public String getCustomers(Request req, Response res) {
-		return "{}";
+		
+		
+		return {};
 	}
 
 	public String getRawMaterials(Request req, Response res) {
